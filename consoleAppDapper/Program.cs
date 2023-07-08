130457a810c2c699d6016b8842d8e249cca80d39
Ulyses Rico Rea <ulyses.rico.2012@gmail.com>
2023-07-07 23:12:54 -0600

    ...

--- consoleAppDapper/Program.cs
+++ consoleAppDapper/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+
+
+DataTable dt = new DataTable();
+dt.Columns.Add("ID");
+
+
+
